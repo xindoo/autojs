82f84095d3c409dee2704bab569bbc4d388d52db
xindoo <xindoo@qq.com>
2019-02-25 19:38:56 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# autojs
+[Auto.js](https://github.com/hyb1996/Auto.js)是一个支持无障碍服务的Android平台上的JavaScript IDE，其发展目标是JsBox和Workflow。 这里我打算基于autojs开发一些有意思的脚本。     
+目前已有 [微信朋友圈自动点赞](微信朋友圈自动点赞.js)，是增进和微信好友间感情的神器。
---
+++ 微信朋友圈自动点赞.js
@@ -0,0 +1,105 @@
+function enterWechatMoment() {
+    app.launch("com.tencent.mm");
+    //确保返回到微信主界面
+    sleep(200);
+    click(692,2073);
+
+    // 连续按多次返回，确保回到网协主界面 
+    for (var i = 0; i < 5; i++) {
+        click(50,140);
+        sleep(100);
+    }
+
+    sleep(50);
+    // 点微信 发现
+    click(692,2073);
+    sleep(50);
+
+    // 点朋友圈入口
+    click(500,300);
+    sleep(1000);
+}
+
+function registEvent() {
+    //启用按键监听
+    events.observeKey();
+    //监听音量上键按下
+    events.onKeyDown("volume_down", function(event){
+        tLog("脚本手动退出");
+        exit();
+    });
+}
+
+function prepare() {
+    // 双击顶部更新朋友圈  
+    click(500, 150);
+    sleep(100);
+    click(500, 150); 
+
+    // 等2s 微信朋友圈更新完成 
+    sleep(2000);
+
+    // 向下滑至第一条朋友圈 
+    swipe(550, 1200, 550, 200, 200);
+
+    if(!requestScreenCapture()){
+        tLog("请求截图失败");
+        exit();
+    }
+}
+
+function clickLike() {
+    if(!requestScreenCapture()){
+        tLog("请求截图失败");
+        exit();
+    }
+    sleep(1000);
+    var image = captureScreen();
+    var arrs = [-1];
+    var x = 991;
+    var defalutColor = -11048043;
+
+    // 扫描出点赞评论按钮的位置，两个点的ARGB颜色为-11048043 
+    for(var y = 250; y < 1500; y++) {
+        var color = images.pixel(image, x, y);
+
+        // 背景色，跳过
+        if (color == -1) {
+            continue;
+        }
+        if (color == defalutColor) {
+            click(x, y)
+            sleep(1000);
+            var clickimage = captureScreen();
+
+            // 如果这个点的颜色是-1，表示这条朋友圈已经点过赞了，返回点赞失败false
+            if (images.pixel(clickimage, 465, y) == -1) {
+                return false;
+                break;
+            }
+            click(523, y)
+            sleep(2000);
+            // 往下滑动到下一条朋友圈的位置  
+            swipe(550, y+200, 550, 150, 200);
+            return true;
+        }
+    }
+}
+
+//程序主入口
+function start(){
+    //注册音量下按下退出脚本监听
+    registEvent();
+    // toast("hello world");
+    enterWechatMoment();
+    prepare();
+    while(true) {
+        // 如果是false，表示已经点到上次点赞的位置了，结束
+        if (clickLike() == false) {
+            break;
+        }
+    }
+    // 返回微信主界面
+    click(50,140);
+}
+start();
